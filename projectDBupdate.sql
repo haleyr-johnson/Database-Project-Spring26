fae20023016ac01b15a34e99181e10a3f67f7edf
Whiton Nguyen <123048292+wdn5349@users.noreply.github.com>
2026-04-14 20:02:36 +0000

    fixed drop tables

--- projectDBupdate.sql
+++ projectDBupdate.sql
@@ -1,1 +1,13 @@
 
+
+SELECT username
+FROM USER
+WHERE membership_type = 'Premium';
+
+INSERT INTO Spring26_S008_T3_USER VALUES (233, 'zendaya13', 'zendaya13@gmail.com', DATE '1996-09-01', 'Other', 'Australia', DATE '2026-04-14', 'Premium');
+
+SELECT username
+FROM Spring26_S008_T3_USER
+WHERE membership_type = 'Premium';
+
+UPDATE USER 
