693e0ffab7a5d91f8aed67ea7fdee1a98f19f2ff
Whiton Nguyen <123048292+wdn5349@users.noreply.github.com>
2026-04-14 20:07:05 +0000

    Added an update query

--- projectDBupdate.sql
+++ projectDBupdate.sql
@@ -1,13 +1,13 @@
-
-
-SELECT username
-FROM USER
-WHERE membership_type = 'Premium';
-
-INSERT INTO Spring26_S008_T3_USER VALUES (233, 'zendaya13', 'zendaya13@gmail.com', DATE '1996-09-01', 'Other', 'Australia', DATE '2026-04-14', 'Premium');
+INSERT INTO Spring26_S008_T3_USER VALUES (233, 'zendaya13', 'zendaya13@gmail.com', DATE '1996-09-01', 'Other', 'Australia', DATE '2026-04-14', 'Free');
 
 SELECT username
 FROM Spring26_S008_T3_USER
-WHERE membership_type = 'Premium';
+WHERE membership_type = 'Premium' AND location = 'United States';
 
-UPDATE USER 
+UPDATE Spring26_S008_T3_USER SET membership_type = 'Premium' WHERE User_ID = 233;
+
+SELECT username
+FROM Spring26_S008_T3_USER
+WHERE membership_type = 'Premium' AND location = 'United States';
+
+DELETE FROM Spring26_S008_T3_USER WHERE User_ID = 233;
