d1234d47d81ae784c4eb63df175f18833ac86095
Whiton Nguyen <123048292+wdn5349@users.noreply.github.com>
2026-04-15 20:16:23 +0000

    Drop and Update

--- projectDBupdate.sql
+++ projectDBupdate.sql
@@ -1,13 +1,14 @@
+-- added Zendaya13 to USER TABLE
 INSERT INTO Spring26_S008_T3_USER VALUES (233, 'zendaya13', 'zendaya13@gmail.com', DATE '1996-09-01', 'Other', 'Australia', DATE '2026-04-14', 'Free');
 
-SELECT username
-FROM Spring26_S008_T3_USER
-WHERE membership_type = 'Premium' AND location = 'United States';
+-- deleted william11 from USER TABLE
+DELETE FROM Spring26_S008_T3_USER WHERE User_ID = 1000;
 
-UPDATE Spring26_S008_T3_USER SET membership_type = 'Premium' WHERE User_ID = 233;
+-- updated nlane's country to Canada from United Kingdom on USER TABLE
+UPDATE Spring26_S008_T3_USER SET Country = 'Canada' WHERE User_ID = 1001;
 
-SELECT username
-FROM Spring26_S008_T3_USER
-WHERE membership_type = 'Premium' AND location = 'United States';
 
-DELETE FROM Spring26_S008_T3_USER WHERE User_ID = 233;
+
+INSERT INTO Spring26_S008_T3_TV_SHOW VALUES (999, 'Greys Anatomy', 'United States', 2005, 'Completed');
+INSERT INTO Spring26_S008_T3_TV_SHOW_GENRE VALUES (999, 'Drama');
+INSERT INTO Spring26_S008_T3_TV_SHOW_LANGUAGE VALUES (169, 'English');
