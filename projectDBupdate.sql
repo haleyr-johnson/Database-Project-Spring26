c3756c5da70a285a2f6bed88ef74058a4da77509
Whiton Nguyen <123048292+wdn5349@users.noreply.github.com>
2026-04-16 01:07:44 -0500

    Updated projectDBupdate.sql

--- projectDBupdate.sql
+++ projectDBupdate.sql
@@ -1,14 +1,29 @@
--- added Zendaya13 to USER TABLE
+-- added Zendaya13 to USER TABLE and updated her location from Australia to Japan
+
 INSERT INTO Spring26_S008_T3_USER VALUES (233, 'zendaya13', 'zendaya13@gmail.com', DATE '1996-09-01', 'Other', 'Australia', DATE '2026-04-14', 'Free');
 
--- deleted william11 from USER TABLE
-DELETE FROM Spring26_S008_T3_USER WHERE User_ID = 1000;
+SELECT Username as Users_From_Australia
+FROM Spring26_S008_T3_USER 
+WHERE Location Like 'Australia';
 
--- updated nlane's country to Canada from United Kingdom on USER TABLE
-UPDATE Spring26_S008_T3_USER SET Country = 'Canada' WHERE User_ID = 1001;
+UPDATE Spring26_S008_T3_USER SET Location = 'Japan' WHERE User_ID = 233;
+
+SELECT Username as Users_From_Australia
+FROM Spring26_S008_T3_USER 
+WHERE Location Like 'Australia';
+
+SELECT Username Users_From_Japan
+FROM Spring26_S008_T3_USER 
+WHERE Location Like 'Japan';
+
+DELETE FROM Spring26_S008_T3_USER WHERE User_ID = 233;
 
 
+-- updated Watchlist (ID = 2018) entry from Futurama to Peaky Blinders, so Futurama appears last in Query 1's results
+UPDATE Spring26_S008_T3_WATCHLIST SET Show_ID = 269 WHERE Watchlist_ID = 2018;
 
-INSERT INTO Spring26_S008_T3_TV_SHOW VALUES (999, 'Greys Anatomy', 'United States', 2005, 'Completed');
-INSERT INTO Spring26_S008_T3_TV_SHOW_GENRE VALUES (999, 'Drama');
-INSERT INTO Spring26_S008_T3_TV_SHOW_LANGUAGE VALUES (169, 'English');
+-- update Banshee's location so it is not present in Query 4's results
+UPDATE Spring26_S008_T3_TV_SHOW SET Country = 'Korea' WHERE Show_ID = 164;
+
+-- update all of Battlestart Galactica's ratings to 0 so appears last in Query 5's results
+UPDATE Spring26_S008_T3_WATCH_LOG SET Rating = 0 WHERE Show_ID = 166;
